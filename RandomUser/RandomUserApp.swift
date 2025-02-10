87cd39f378bfc226934fb182716c7a69700a7bcb
Sadegh <sadegh@storelab.app>
2025-02-10 09:39:07 +0000

    added Xcode project

---
+++ RandomUser/RandomUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserApp.swift
+//  RandomUser
+//
+//  Created by Sadegh on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RandomUserTests/RandomUserTests.swift
@@ -0,0 +1,16 @@
+//
+//  RandomUserTests.swift
+//  RandomUserTests
+//
+//  Created by Sadegh on 10/02/2025.
+//
+
+import Testing
+
+struct RandomUserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
